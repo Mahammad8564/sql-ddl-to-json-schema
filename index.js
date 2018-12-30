cf13abc3a631819fe0e3ccf65a4ee0f323a1bcd6
Mahammad <mahammad.c@productivet.com>
2018-12-30 22:12:44 +0530

    push

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const lib = require('./lib');
 
 module.exports = lib;
+
+// var sql = `CREATE TABLE users (
+//   id INT(11) NOT NULL AUTO_INCREMENT,
+//   nickname VARCHAR(255) NOT NULL,
+//   deleted_at TIMESTAMP NULL,
+//   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+//   updated_at TIMESTAMP,
+//   PRIMARY KEY (id)
+// ) ENGINE MyISAM COMMENT 'All system users';
+
+// ALTER TABLE users ADD UNIQUE KEY unq_nick (nickname);`
+
+// var parser = new lib();
+
+// parser.feed(sql)
+//   .toMongooseSchemaFiles(__dirname)
+//   .then((outputFilePaths) => {
+//     // ...
+//   });
